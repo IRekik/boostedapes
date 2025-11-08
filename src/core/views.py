68b9c7843e8533e34cfafc18cc3a5a2c37699a8d
abdellahcs <155846993+abdellahcs@users.noreply.github.com>
2025-11-08 12:36:55 -0500

    Update views.py

--- src/core/views.py
+++ src/core/views.py
@@ -2,7 +2,7 @@ import requests
 from django.shortcuts import render
 from django.http import JsonResponse
 
-API_KEY = "RGAPI-80652536-f4e6-4f53-9d3b-7ba0761d49bc"
+API_KEY = "RGAPI-70537a25-57fd-45f8-82dc-57637f9b2a8b"
 
 def home(request):
     return render(request, "core/home/index.html")
@@ -16,7 +16,7 @@ def api_games(request):
     account = get_account(username,tag)
     puuid = account.get("puuid")
 
-    api_url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key=RGAPI-80652536-f4e6-4f53-9d3b-7ba0761d49bc"
+    api_url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20"
     api_url += "&api_key=" + API_KEY
     
     res = requests.get(api_url)
