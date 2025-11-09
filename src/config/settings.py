912100de5417bfcd1f41ad722e15d88a5cbcdfaf
Ismael Rekik <65619960+IRekik@users.noreply.github.com>
2025-11-09 17:36:30 +0900

    Load env variables to settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RIOT_API_KEY = os.getenv("RIOT_API_KEY")
